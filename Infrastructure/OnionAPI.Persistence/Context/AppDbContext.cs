d4832d93262ac093e133b81acca45dfa39c6feae
Umut Ozcan <umut.yozcan@gmail.com>
2024-02-24 23:49:12 +0300

    EfCore ile DbContext oluşturuldu

---
+++ Infrastructure/OnionAPI.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionAPI.Domain.Entities;
+using System.Reflection;
+
+namespace OnionAPI.Persistence.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext()
+    { }
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    { }
+
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Detail> Details { get; set; }
+
+    // coka cok iliskiyi ef kendisi oluşturacak
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
